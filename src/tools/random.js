97623c13532b47ed887c01dc15a57a5a2e2fd2bc
iskaldvind <iskaldvind@gmail.com>
2017-05-02 02:00:39 +0300

    Moved from Math.random to seedrandom

--- src/tools/random.js
+++ src/tools/random.js
@@ -1,4 +1,8 @@
+import seedrandom from 'seedrandom';
+
+const rng = seedrandom();
+
 export const int = (startingInteger, endingInteger) =>
-Math.floor(Math.random() * ((endingInteger - startingInteger) + 1)) + startingInteger;
+Math.floor(rng() * ((endingInteger - startingInteger) + 1)) + startingInteger;
 
 export const choice = array => array[int(0, array.length - 1)];
