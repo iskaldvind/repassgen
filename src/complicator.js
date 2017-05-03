0dab9c55981867f400e90539716214f5ec0961c0
iskaldvind <iskaldvind@gmail.com>
2017-05-03 23:55:14 +0300

    Got tired

--- src/complicator.js
+++ src/complicator.js
@@ -1,1 +1,55 @@
+import * as Random from './tools/random';
 
+const minComplexifyReplacementsPerType = 1;
+const complexityRate = 0.1;
+
+const compCaps = letter => letter.toUpperCase();
+const compNums = () => Random.choice(['2', '3', '4', '6', '7', '8', '9']);
+const compSyms = () => Random.choice(['!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '?']);
+const complexityMatrix = { ASNF: compCaps, nNfF: compNums, sSfF: compSyms };
+const refineMatrix = { l: ['n', 'm'], S: ['H', 'F'], I: ['A', 'E', 'U'], O: ['A', 'E', 'U'] };
+
+const refine = password => password.split('').map((symbol) => {
+  if (refineMatrix[symbol]) {
+    return Random.choice(refineMatrix[symbol]);
+  }
+  return symbol;
+}).join('');
+
+const multiplyArray = (array, times, timesLeft, acc) => {
+  const currentTimesLeft = timesLeft || times;
+  const currentAcc = acc || [];
+  if (timesLeft === 0) {
+    return acc;
+  }
+  return multiplyArray(array, times, currentTimesLeft - 1, [...currentAcc, ...array]);
+};
+
+const getFunctionsToAppply = (options, replacementsPerType) =>
+  multiplyArray(Object.keys(complexityMatrix)
+    .filter(key => key.indexOf(options.passComplexity) > -1)
+    .map(key => complexityMatrix[key]), replacementsPerType);
+
+const modify = (options, password) => {
+  const replacementsPerType = Math.max(minComplexifyReplacementsPerType,
+    Math.floor(password.length * complexityRate));
+  const functionsToApplySequence = getFunctionsToAppply(options, replacementsPerType);
+  if (functionsToApplySequence.length === 0) {
+    return password;
+  }
+  const splittedPassword = password.split('').reduce((acc, letter) => [...acc, [acc.length, 'um', letter]], []);
+  const modifiedPassword = functionsToApplySequence.reduce((symbolData, appliedFunction) => {
+      const unmodifiedLettersIndices = symbolData.reduce((acc, entry) => {
+        if (entry[1] === 'um') {
+          return [...acc, entry[0]];
+        }
+        return acc;
+      }, []);
+      const indexToModify = Random.choice(unmodifiedLettersIndices);
+      const letterToModify = symbolData[indexToModify][2];
+      const newSymbol = appliedFunction(letterToModify);
+      return [...symbolData.slice(0, indexToModify), [indexToModify, 'mo', newSymbol], ...symbolData.slice(indexToModify + 1)];
+    },
+    splittedPassword);
+  return modifiedPassword.map(symbolData => symbolData[2]).join('');
+};
--- src/generator.js
+++ src/generator.js
@@ -1,60 +1,8 @@
 import * as Random from './tools/random';
 
 const maxGenerationTries = 100;
-const minComplexifyReplacementsPerType = 1;
-const complexityRate = 0.1;
 
-const compCaps = letter => letter.toUpperCase();
-const compNums = () => Random.choice(['2', '3', '4', '6', '7', '8', '9']);
-const compSyms = () => Random.choice(['!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '?']);
-const complexityMatrix = { ASNF: compCaps, nNfF: compNums, sSfF: compSyms };
-const refineMatrix = { l: ['n', 'm'], S: ['H', 'F'], I: ['A', 'E', 'U'], O: ['A', 'E', 'U'] };
-
-
-const refine = password => password.split('').map((symbol) => {
-  if (refineMatrix[symbol]) {
-    return Random.choice(refineMatrix[symbol]);
-  }
-  return symbol;
-}).join('');
-
-const multiplyArray = (array, times, timesLeft, acc) => {
-  const currentTimesLeft = timesLeft || times;
-  const currentAcc = acc || [];
-  if (timesLeft === 0) {
-    return acc;
-  }
-  return multiplyArray(array, times, currentTimesLeft - 1, [...currentAcc, ...array]);
-};
-
-const getFunctionsToAppply = (options, replacementsPerType) =>
-  multiplyArray(Object.keys(complexityMatrix)
-    .filter(key => key.indexOf(options.passComplexity) > -1)
-    .map(key => complexityMatrix[key]), replacementsPerType);
-
-const modify = (options, password) => {
-  const replacementsPerType = Math.max(minComplexifyReplacementsPerType,
-    Math.floor(password.length * complexityRate));
-  const functionsToApplySequence = getFunctionsToAppply(options, replacementsPerType);
-  if (functionsToApplySequence.length === 0) {
-    return password;
-  }
-  const splittedPassword = password.split('').reduce((acc, letter) => [...acc, [acc.length, 'um', letter]], []);
-  const modifiedPassword = functionsToApplySequence.reduce((symbolData, appliedFunction) => {
-    const unmodifiedLettersIndices = symbolData.reduce((acc, entry) => {
-      if (entry[1] === 'um') {
-        return [...acc, entry[0]];
-      }
-      return acc;
-    }, []);
-    const indexToModify = Random.choice(unmodifiedLettersIndices);
-    const letterToModify = symbolData[indexToModify][2];
-    const newSymbol = appliedFunction(letterToModify);
-    return [...symbolData.slice(0, indexToModify), [indexToModify, 'mo', newSymbol], ...symbolData.slice(indexToModify + 1)];
-  },
-    splittedPassword);
-  return modifiedPassword.map(symbolData => symbolData[2]).join('');
-};
+function Gen
 
 const buildInitialSequence = (data) => {
   const head = Random.choice(['vovels', 'consonants']) === 'vovels' ?
@@ -110,8 +58,8 @@ const generator = (options, data) => {
   return refinedPasswords.reduce((acc, password) => `${acc}${password}\n`, '');
 };
 
-const generate = (options, data) => {
-  console.log(generator(options, data));
+const generate = (requiredLength, genData) => {
+
 };
 
 export default generate;
