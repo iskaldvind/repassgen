9de0371abcc30b886b450c6cafa47af353205847
iskaldvind <iskaldvind@gmail.com>
2017-05-03 22:39:51 +0300

    Split generator from complicator

--- src/tools/random.js
+++ src/tools/random.js
@@ -1,8 +1,8 @@
 import seedrandom from 'seedrandom';
 
-const rng = seedrandom();
+const getRandom = () => seedrandom(Date.now());
 
 export const int = (startingInteger, endingInteger) =>
-Math.floor(rng() * ((endingInteger - startingInteger) + 1)) + startingInteger;
+Math.floor(getRandom() * ((endingInteger - startingInteger) + 1)) + startingInteger;
 
 export const choice = array => array[int(0, array.length - 1)];
